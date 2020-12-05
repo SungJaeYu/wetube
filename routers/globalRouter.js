9e111802b7e1c1d07d4326497ec6cf2da085b162
SungJaeYu <ysj7045@naver.com>
2020-12-05 20:13:53 +0900

    Authentication : Login and Join,( session, cookie, mongostore )

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,16 +1,20 @@
 import express from "express";
 import { getJoin, postJoin, logout, getLogin, postLogin } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
+import { onlyPrivate, onlyPublic } from "../middlewares";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin);
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
-globalRouter.get(routes.logout, logout);
+
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
+
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
+
+globalRouter.get(routes.logout, onlyPrivate, logout);
 
 export default globalRouter;
